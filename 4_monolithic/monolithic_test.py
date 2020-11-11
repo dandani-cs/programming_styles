1ff20e6b2bdb5b9ba70a9faeb557c9bd8d4f7e17
dani-cs <dani.cstech@gmail.com>
2020-11-11 13:29:15 +0800

    201111 chapter 3 finished, chapter 4 started

---
+++ 4_monolithic/monolithic_test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import os
+import re
+from subprocess import getstatusoutput, getoutput
+
+prg = ".\\monolithic.py"
+
+# --------------------------------------------------
+def test_exists():
+    """Checks if program exists"""
+
+    assert os.path.isfile(prg)
+
+# --------------------------------------------------
+def test_input_file():
+    """Checks if output is correct for input_file.txt"""
+
+    out = getoutput(f'{prg} ..\\input_file.txt')
+    expected = """live - 2
+mostly - 2
+white - 1
+tigers - 1
+india - 1
+wild - 1
+lions - 1
+africa - 1"""
+    assert out.strip() == expected
