9b3a29023cefcf5defe8389a353d61e1da108b3d
Danielle Samonte <dcsamonte@gmail.com>
2021-07-03 10:23:45 +0800

    chapter 25 finsihed and tested

---
+++ 25_quarantine/quarantine_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import os
+import re
+import sys
+from subprocess import getstatusoutput, getoutput
+
+prg = "quarantine.py"
+
+# --------------------------------------------------
+def test_exists():
+    """Checks if program exists"""
+
+    assert os.path.isfile(prg)
+
+# --------------------------------------------------
+def test_first_line():
+    """Checks if output is correct for input_file.txt"""
+
+    out = getoutput(f'python {prg} ../input_file.txt')
+    expected = "2"
+
+    assert out.split('\n')[0][-1] == expected
+
+# ------------------------------------------------------
+def test_sort():
+    """Checks that the order of printing is from highest frequency to lowest"""
+
+    out = getoutput(f'python {prg} ../input_file.txt')
+    out.split('\n')
+
+    basis = 'live - 2'
+    to_find = 'africa - 1'
+
+    assert to_find in out[out.index(basis) + 1:]
